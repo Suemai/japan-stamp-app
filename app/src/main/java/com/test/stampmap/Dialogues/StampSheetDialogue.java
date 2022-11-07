03ae7c7d977238388f5eafd23834692dbd08ce7e
Suemai <69875258+Suemai@users.noreply.github.com>
2022-11-07 12:26:51 +0900

    Added stamp bottom dialogue ui

--- app/src/main/java/com/test/stampmap/Dialogues/StampSheetDialogue.java
+++ app/src/main/java/com/test/stampmap/Dialogues/StampSheetDialogue.java
@@ -1,4 +1,19 @@
 package com.test.stampmap.Dialogues;
 
-public class StampSheetDialogue {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.test.stampmap.R;
+
+public class StampSheetDialogue extends BottomSheetDialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, @Nullable ViewGroup container, @Nullable Bundle savedInstance) {
+        View v = layoutInflater.inflate(R.layout.filter_drawer, container, false);
+
+        return v;
+    }
 }
