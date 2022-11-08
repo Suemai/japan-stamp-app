8129aa398a25fcc3f00a144a2939925c6b205466
royston thorndycraft <18059207@brookes.ac.uk>
2022-11-08 18:03:28 +0900

    converted to ListView

--- app/src/main/java/com/test/stampmap/Dialogues/StampSheetDialogue.java
+++ app/src/main/java/com/test/stampmap/Dialogues/StampSheetDialogue.java
@@ -4,16 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.widget.*;
 import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.divider.MaterialDivider;
 import com.test.stampmap.R;
 import com.test.stampmap.Stamp.Stamp;
 import com.test.stampmap.Stamp.StampSet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StampSheetDialogue extends BottomSheetDialogFragment {
 
@@ -37,15 +37,19 @@ public class StampSheetDialogue extends BottomSheetDialogFragment {
         holiday.setText("Holiday: " + stampSet.getHoliday());
         fee.setText("Entry Fee: " + stampSet.getEntryFee());
 
-        LinearLayout stampList = v.findViewById(R.id.stamp_list);
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
         int i = 1;
-        for (Stamp stamp : stampSet) {
-            View element = layoutInflater.inflate(R.layout.stamp_element, null, false);
-            ((TextView)element.findViewById(R.id.stampNo_drawer)).setText("Stamp: " + i++);
-            ((TextView)element.findViewById(R.id.obtainable_drawer)).setText("Obtainable: " + (stamp.getIsObtainable() ? "Yes" : "No"));
-            ((TextView)element.findViewById(R.id.location_drawer)).setText("Location: " + stamp.getLocation());
-            stampList.addView(element);
+        for (Stamp stamp : stampSet){
+            HashMap<String, String> map = new HashMap<>();
+            map.put("num", "Stamp " + i++);
+            map.put("obtain", "Obtainable: " + (stamp.getIsObtainable() ? "Yes" : "No"));
+            map.put("location", "Location: " + stamp.getLocation());
+            list.add(map);
         }
+        ((ListView)v.findViewById(R.id.stamp_list)).setAdapter(
+                new SimpleAdapter(requireContext(), list, R.layout.stamp_element,
+                    new String[]{"num", "obtain", "location"},
+                    new int[]{R.id.stampNo_drawer, R.id.obtainable_drawer, R.id.location_drawer}));
         return v;
     }
 }
