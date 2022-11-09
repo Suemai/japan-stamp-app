eb1afa3b8e03dd1162949a775cb50a30e99493ac
Suemai <69875258+Suemai@users.noreply.github.com>
2022-11-09 11:15:53 +0900

    Added fragments for the MyStamps page
    Nothing has been properly implemented yet

---
+++ app/src/main/java/com/test/stampmap/Activity/MyStampsActivity.java
@@ -0,0 +1,14 @@
+package com.test.stampmap.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.test.stampmap.R;
+
+public class MyStampsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_stamps);
+    }
+}
